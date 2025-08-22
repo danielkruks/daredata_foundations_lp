c89b39cfa2beee7212d3781264a2b468ba25cc69
danielkruks <danielkruk79@gmail.com>
2025-08-22 01:41:59 +0100

    Creation of mock fixtures files using create_fixture.py and inclusion in conftest.py

--- life_expectancy/tests/conftest.py
+++ life_expectancy/tests/conftest.py
@@ -1,31 +1,27 @@
 """Pytest configuration file"""
 import pandas as pd
 import pytest
+from pathlib import Path
 
 from . import FIXTURES_DIR, OUTPUT_DIR
 
 
-@pytest.fixture(autouse=True)
-def run_before_and_after_tests() -> None:
-    """Fixture to execute commands before and after a test is run.
+@pytest.fixture
+def raw_life_expectancy_eu_file():
+    '''
+    Defining the raw life expectancy sample file as a fixture
+    '''
+    fixture_path = Path(__file__).parent / "fixtures" / "eu_life_expectancy_raw.tsv"
+    return pd.read_csv(fixture_path, sep="\t")
 
-    1. Everything you may write before 'yield' will run before the tests
-    2. The command 'yield' marks where tests will happen
-    3. The code after 'yield' will be run after the tests are finished.
 
-    Before you start using your own fixtures, all tests will produce a csv file
-    on the output directory. Since this is bad practise, we are erasing this
-    file to avoid polluting your workspace.
-
-    After refactoring your functions this code will do nothing.
-    """
-    # Setup: fill with any logic you want
-
-    yield # this is where the testing happens
-
-    # Teardown : fill with any logic you want
-    file_path = OUTPUT_DIR / "pt_life_expectancy.csv"
-    file_path.unlink(missing_ok=True)
+@pytest.fixture
+def expected_life_expectancy_eu_file():
+    '''
+    Defining the sample expected output file as a fixture
+    '''
+    fixture_path = Path(__file__).parent / "fixtures" / "eu_life_expectancy_expected.csv"
+    return pd.read_csv(fixture_path)
 
 
 @pytest.fixture(scope="session")
